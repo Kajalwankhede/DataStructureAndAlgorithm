0a57c1627c3dd233ae88dddbe8df98111476d86e
Kajakwankhede <Kajalwankhede@example.com>
2022-11-08 21:57:21 +0530

    Program 7- To get prime number range between 1 to 1000.

--- src/com/datastructure/PrimeNumber.java
+++ src/com/datastructure/PrimeNumber.java
@@ -3,5 +3,23 @@ package com.datastructure;
 public class PrimeNumber {
     public static void main(String[] args) {
         System.out.println("To get 1 to 1000 prime numbers:");
+            int lower = 0, upper = 1000;
+            for (int i = lower; i <= upper; i++)
+                if (isPrime (i))
+                    System.out.println (i);
+        }
+        static boolean isPrime (int n)
+        {
+            int count = 0;
+            if (n < 2)   // 0, 1 negative numbers are not prime
+                return false;
+
+            for (int i = 2; i < n; i++) // checking the number of divisors between 1 and the number n-1
+            {
+                if (n % i == 0)
+                    return false;
+            }
+            return true; // if reached here then must be true
+        }
     }
-}
+
