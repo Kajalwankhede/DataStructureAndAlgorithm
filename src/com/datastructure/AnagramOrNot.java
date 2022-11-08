b3b8764087e374558d5e482dfa57e9e113cf22e2
Kajakwankhede <Kajalwankhede@example.com>
2022-11-08 22:10:56 +0530

    Anangram or not

---
+++ src/com/datastructure/AnagramOrNot.java
@@ -0,0 +1,7 @@
+package com.datastructure;
+
+public class AnagramOrNot {
+    public static void main(String[] args) {
+        System.out.println("Checking Two string are anagram or not");
+    }
+}
